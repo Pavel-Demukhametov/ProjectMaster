052a59a5e3a077a486805a11be246979e5e9107d
IvanKhromov <iv.khromov7@gmail.com>
2024-03-01 21:43:17 +0500

    Добавление API для модели студента на чтение и создание. Для просмотра к основному url добавить api/

---
+++ ProjectMaster/Users/routers.py
@@ -0,0 +1,5 @@
+from rest_framework import routers
+from .views import StudentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'students', StudentViewSet)
--- ProjectMaster/Users/views.py
+++ ProjectMaster/Users/views.py
@@ -1,3 +1,12 @@
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializer
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
