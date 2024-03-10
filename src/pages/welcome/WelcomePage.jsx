48055dd3bb239244a7746fa7006419b976554899
Павел Демухаметов <pavdemuz@gmail.com>
2024-03-10 18:24:49 +0500

    Изменить WelcomePage под ProjectMaster

--- src/pages/welcome/WelcomePage.jsx
+++ src/pages/welcome/WelcomePage.jsx
@@ -25,22 +25,22 @@ const WelcomePage = () => {
           </header>
 
           
-          <main className="mt-[10%] w-full ">
+          <main className="mt-[10%] w-full h-[100vh] ">
             <div className="pb-[340px] md:pb-[240px] sm:pb-[270px] flex flex-col items-center justify-center text-center sm:items-start sm:justify-start sm:text-left md:items-center md:justify-center md:text-center lg:items-start lg:justify-start lg:text-left">
               <div className="flex flex-col justify-center md:justify-center lg:justify-start">
-                <h1 className="text-7xl sm:text-[120px] md:text-[120px] font-bold text-transparent bg-clip-text gradient-text animated-gradient leading-normal">
-                  ProjectHub
+                <h1 className="text-6xl sm:text-[120px] md:text-[120px] font-bold text-transparent bg-clip-text gradient-text animated-gradient leading-normal">
+                  ProjectMaster
                 </h1>
                 <div className="text-2xl  dark:text-trueWhite text-customGray pb-12 " style={{ maxWidth: '600px' }}>
-                  Цифровой архив студенческих проектов в области информационных технологий.
+                  Веб-приложение для управления студенческими проектами.
                 </div>
               </div>
-              <div className="flex flex-col gap-10 pt-6 w-full md:w-1/2 pl-7 pr-9 ">
-                <a href="/projects/" className="no-underline text-center text-2xl px-6 py-3 bg-[#fc9af7] dark:bg-[#d66bd1] hover:bg-[#d66bd1] dark:hover:bg-[#fc9af7] text-customGray dark:text-trueWhite hover:text-trueWhite dark:hover:text-customGray font-semibold rounded-md transition duration-300">
+              <div className="flex flex-col gap-10 pt-6 w-full md:w-1/2  ">
+                <a href="/projects/" className="no-underline text-center text-3xl px-6 py-3 bg-[#fc9af7] dark:bg-[#d66bd1] hover:bg-[#d66bd1] dark:hover:bg-[#fc9af7] text-customGray dark:text-trueWhite hover:text-trueWhite dark:hover:text-customGray font-semibold rounded-md transition duration-300">
                   Каталог
                 </a>
-                <a href="/api" className="no-underline text-center text-2xl px-6 py-3 bg-[#89abfc] dark:bg-[#4b6cb7] hover:bg-[#4b6cb7] dark:hover:bg-[#89abfc] text-customGray dark:text-trueWhite hover:text-trueWhite dark:hover:text-customGray font-semibold rounded-md transition duration-300">
-                  API
+                <a href="/api" className="no-underline text-center text-3xl px-6 py-3 bg-[#89abfc] dark:bg-[#4b6cb7] hover:bg-[#4b6cb7] dark:hover:bg-[#89abfc] text-customGray dark:text-trueWhite hover:text-trueWhite dark:hover:text-customGray font-semibold rounded-md transition duration-300">
+                Зарегистрироваться
                 </a>
                 
               </div>
