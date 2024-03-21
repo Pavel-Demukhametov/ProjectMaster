6f4c4e8c1a9b49fa13c1803243f95ddf2a647aa2
Павел Демухаметов <pavdemuz@gmail.com>
2024-03-21 22:00:00 +0500

    Закрыть routes для незарегестрированных пользователей и разных аккаунтов

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './layout/Layout';
 import NotFound from './pages/notFound/NotFoundPage';
 import WelcomePage from './pages/welcome/WelcomePage';
@@ -10,25 +10,42 @@ import StudentsCatalogPage from './pages/studentsCatalog/StudentsCatalogPage';
 import Catalog from './pages/projectCatalog/ProjectCatalogPage';
 import OpenProjectCatalog from './pages/projectCatalog/OpenProjectPage';
 
+// Функция проверки аутентификации и роли
+const requireAuthAndRole = (component, role) => {
+  const isAuthenticated = localStorage.getItem('accessToken');
+  const userRole = localStorage.getItem('userRole'); 
+  if (isAuthenticated && userRole === role) {
+    return component;
+  } else if (isAuthenticated){
+    return <Navigate to="/NotFound" />;
+  }
+  else {
+    return <Navigate to="/Login" />; 
+  }
+};
+
 function App() {
   return (
+    <Router>
+      <div className="App bg-lightHeader dark:bg-darkGray min-w-[350px] min-h-screen max-h-200px">
+        <Routes>
+          <Route index element={<WelcomePage />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="SignUp/" element={<SignUpPage />} />
+            <Route path="Login/" element={<LoginPage />} />
 
-      <Router>
-        <div className="App bg-lightHeader dark:bg-darkGray min-w-[350px] min-h-screen max-h-200px">
-          <Routes>
-            <Route index element={<WelcomePage />} />
-            <Route path="/" element={<Layout />}>
-              <Route path="SignUp/" element={<SignUpPage />}/>
-              <Route path="Login/" element={<LoginPage />}/>
-              <Route path="CreateProject/" element={<CreateProjectPage />}/>
-              <Route path="Students/" element={<StudentsCatalogPage />}/>
-              <Route path="Projects/" element={<Catalog />}/>
-              <Route path="Join-project/" element={<OpenProjectCatalog />}/>
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </div>
-      </Router>
+            <Route path="CreateProject/" element={requireAuthAndRole(<CreateProjectPage />, 'CURATOR')} />
+
+            <Route path="Students/" element={requireAuthAndRole(<StudentsCatalogPage />, 'CURATOR')} />
+
+            <Route path="Join-project/" element={requireAuthAndRole(<OpenProjectCatalog />, 'STUDENT')} />
+            <Route path="Projects/" element={<Catalog />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
