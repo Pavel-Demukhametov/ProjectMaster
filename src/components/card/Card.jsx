44a20d0e5c029b5386e7742bddad38a48506a218
Павел Демухаметов <pavdemuz@gmail.com>
2024-03-21 21:55:37 +0500

    Убрать абсолютное позицирование в card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ id, project_name, description, created_date, status }) => {
   return (
-    <article className="w-full group p-4 rounded-md shadow-md bg-white dark:bg-gray-800 hover:bg-gray-100 hover:dark:bg-gray-700 relative">
-      <div className="absolute right-4 top-4 text-sm text-gray-500 dark:text-gray-300">
-        {status}
-      </div>
-      <div className="text-sm text-gray-500 dark:text-gray-300 mb-2">
-        {new Date(created_date).toLocaleDateString()}
+    <article className="w-full p-4 rounded-md shadow-md bg-white dark:bg-gray-800 hover:bg-gray-100 hover:dark:bg-gray-700 flex flex-col justify-between">
+      <div className='flex justify-between'>
+        <div className="text-sm text-gray-500 dark:text-gray-300 mb-2">
+          {new Date(created_date).toLocaleDateString()}
+        </div>
+        <div className="self-end text-sm text-gray-500 dark:text-gray-300">
+          {status}
+        </div>
       </div>
       <Link to={`/projects/${id}`} className="no-underline">
         <div>
@@ -20,6 +22,7 @@ const Card = ({ id, project_name, description, created_date, status }) => {
           </p>
         </div>
       </Link>
+    
     </article>
   );
 };
